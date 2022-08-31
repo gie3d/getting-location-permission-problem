c0e42c73a3ddd2878c577d3ed75a420eb2d309c8
gie <gie68i@gmail.com>
2022-08-31 18:15:14 +0700

    Initial Commit

---
+++ testswiftui/testswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testswiftuiApp.swift
+//  testswiftui
+//
+//  Created by gie on 31/8/22.
+//
+
+import SwiftUI
+
+@main
+struct testswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
